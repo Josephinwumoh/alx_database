186c13c2c74245b4c904170d1ea5e926ed503688
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-14 02:37:33 +0100

    Print Full Table.

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- 5-full_table.sql
 
--- Prints the full description for table first_table
-
--- From the database 'hbtn_0c_0'
-SHOW CREATE TABLE first_table;
+-- Prints the full description for table 'first_table'
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
