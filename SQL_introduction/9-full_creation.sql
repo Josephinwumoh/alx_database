d57529046dddaf66cb6b7cfd0bef3a4f7cca3e7d
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-14 04:02:30 +0100

    second_table.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -19,7 +19,7 @@ INSERT INTO second_table (id, name, score)
 
 VALUES
 
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "BOb", 14),
-(4, "George", 8);
+(1, “John”, 10),
+(2, “Alex”, 3),
+(3, “Bob”, 14),
+(4, “George”, 8);
