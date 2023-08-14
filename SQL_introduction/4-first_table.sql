2a2938bc0a37eee20219b3b56a838a9fd94148f0
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-14 03:04:15 +0100

    Full tables.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -5,4 +5,4 @@
 -- id INT
 
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table(id INT(11), name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -3,3 +3,7 @@
 -- Prints the full description for table 'first_table'
 
 SHOW CREATE table first_table;
+
+SELECT CREATE_TABLE
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
