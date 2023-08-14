729857e740f7e88c7e3bec7713eb5143f5a2c621
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-14 03:54:49 +0100

    Second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,11 +4,23 @@
 
 -- id = INT, name = VARCHAR(256), score INT
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table ( 
+id INT,
+
+name VARCHAR(256),
+
+score INT
+
+);
 
 -- Create maximum of 4 records
 
-INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
-INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO second_table(id, name, score) VALUES (3, "BOb", 14);
-INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table
+
+(id, name, score) 
+
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "BOb", 14),
+(4, "George", 8);
