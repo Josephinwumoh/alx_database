2f1bf09c8cb0b39c9a0a95570e5fd02e1afca267
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-17 14:29:49 +0100

    The database hbtn_0d_usa and the table states.

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create the database hbtn_0d_usa and the table states.
+
+CREATE THE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE THE TABLE IF NOT EXISTS hbtn_0d_usa.states
+    (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+    
+);
