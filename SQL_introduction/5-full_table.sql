f5977ac937010fa1e707fb6f937d5d4c3f049550
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-17 09:48:42 +0100

    SQL_more_queries.

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -3,7 +3,3 @@
 -- Prints the full description for table 'first_table'
 
 SHOW CREATE table first_table;
-
-SELECT CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
