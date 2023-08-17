17fc4e36a42a7bf58a3862eec2543e7f578901a3
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-17 14:39:49 +0100

    Create the database hbtn_0d_usa.

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,8 +2,14 @@
 
 CREATE THE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database hbtn_0d_usa.
+
+USE hbtn_0d_usa;
+
+-- Create the table state inside hbtn_0d_usa.
+
 CREATE THE TABLE IF NOT EXISTS states
-    (id INT NOT NULL DEFAULT 1, UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
     
 );
