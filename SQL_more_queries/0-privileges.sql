64b91540cab52fa1d8e69cbb6ac25acc38347edd
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-17 10:58:31 +0100

    List Privileges.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,10 +1,7 @@
--- Create user_0d_1 and user_od_2.
-
+-- lists all privileges of mysql for users user_0d_1 and user_0d_2
 CREATE USER 'user_0d_1'@'localhost';
-
 CREATE USER 'user_0d_2'@'localhost';
 
---GRANT ALL PRIVELEGES TO USERS
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- Grant All privileges
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
